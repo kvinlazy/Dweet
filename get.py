0e755ec3cdc268f9599dedcaaf58d28e959d5b74
kvinlazy <kevinnaik@gmail.com>
2017-05-21 20:38:48 +0530

    Add files via upload

---
+++ get.py
@@ -0,0 +1,13 @@
+import os
+import json
+import urllib2
+
+
+os.system('cls')
+url = 'http://dweet.io:443/get/latest/dweet/for/AU_test'    
+req = urllib2.Request(url)
+response = urllib2.urlopen(req)
+x = response.read()
+x =json.loads(x)
+print response
+
---
+++ send.py
@@ -0,0 +1,14 @@
+import urllib
+import urllib2
+import random
+import string
+
+for i in range(100):
+    x = random.randint(0, 1000)
+    y = str(x)
+    url = 'https://dweet.io:443/dweet/for/AU_test'
+    data = urllib.urlencode({'Random' : y})
+    req = urllib2.Request(url, data)
+    response = urllib2.urlopen(req)
+    re = response.read()
+    print re
